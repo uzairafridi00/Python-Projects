b143cdf5b9f0f5efb43393df7dceed6841acfd57
UzairAfridi <uzairafridi00@gmail.com>
2020-12-20 09:08:45 +0500

    Python Script to Download Instagram Profile Photo

---
+++ instaProfileDownloader.py
@@ -0,0 +1,15 @@
+import os
+import instaloader
+
+
+def downloader(username):
+    instance = instaloader.Instaloader()
+    # Change working directory to Desktop
+    os.chdir(os.path.join(os.path.expanduser("~"), "Desktop"))
+    return instance.download_profile(username, profile_pic_only=True)
+
+
+if __name__ == "__main__":
+    print("Enter username: ")
+    username = input(r" >>")
+    downloader(username)
